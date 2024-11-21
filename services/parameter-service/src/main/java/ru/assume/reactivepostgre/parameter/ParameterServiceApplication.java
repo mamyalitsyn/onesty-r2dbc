872606d458ad8bf838fd55ac6d953e2a09e6a95b
Mikhail Myalitsyn <myalitsyn.ma@gmail.com>
2024-11-21 13:35:37 +0300

    feat: lost files for previous commit

---
+++ services/parameter-service/src/main/java/ru/assume/reactivepostgre/parameter/ParameterServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.assume.reactivepostgre.parameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParameterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParameterServiceApplication.class, args);
+	}
+
+}
---
+++ services/parameter-service/src/test/java/ru/assume/reactivepostgre/parameter/ParameterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.assume.reactivepostgre.parameter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParameterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
